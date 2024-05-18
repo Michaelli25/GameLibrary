7f6bb1494aad3ff9906ee60ce49072374a5e89bf
Michael Li <michaelxli25@gmail.com>
2024-05-18 17:40:51 -0400

    fixed some issues with mybatis being recognized

--- src/main/java/com/michaelli25/GameLibrary/GameLibraryApplication.java
+++ src/main/java/com/michaelli25/GameLibrary/GameLibraryApplication.java
@@ -2,8 +2,12 @@ package com.michaelli25.gamelibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.michaelli25.gamelibrary.restcontroller", "com.michaelli25.gamelibrary.business.service", "com.michaelli25.gamelibrary.integration"})
+@MapperScan("com.michaelli25.gamelibrary.integration.mapper")
 public class GameLibraryApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/michaelli25/GameLibrary/business/service/GameLibraryServiceImpl.java
+++ src/main/java/com/michaelli25/GameLibrary/business/service/GameLibraryServiceImpl.java
@@ -3,10 +3,14 @@ package com.michaelli25.gamelibrary.business.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.michaelli25.gamelibrary.business.GameLibrary;
 import com.michaelli25.gamelibrary.integration.GameLibraryDao;
 
+@Service
+@Transactional
 public class GameLibraryServiceImpl implements GameLibraryService{
     @Autowired
     private GameLibraryDao dao;
@@ -24,8 +28,14 @@ public class GameLibraryServiceImpl implements GameLibraryService{
 
     @Override
     public GameLibrary getGameByTitle(String title) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGameByTitle'");
+        GameLibrary game = null;
+        try {
+            game = dao.getGameByTitle(title);
+        } catch (Exception e) {
+            String errorMessage = String.format("Error querying the database for a game with id of %s. Please try again later.", title);
+            throw new IllegalStateException(errorMessage, e);
+        }
+        return game;
     }
     
 }
